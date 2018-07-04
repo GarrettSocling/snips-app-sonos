9c1c269c1c2823ea4d97a57c5917710c1b9d33b3
paufabregat <pau.fabregat.p@gmail.com>
2018-07-04 15:07:11 +0200

    Add method in MusicService to set the node query builder and cascade changes

--- snipssonos/use_cases/play/artist.py
+++ snipssonos/use_cases/play/artist.py
@@ -14,6 +14,7 @@ class PlayArtistUseCase(UseCase):
     def process_request(self, request_object):
 
         device = self.device_discovery_service.get()
+        self.music_search_service.set_node_query_builder(device.name)
 
         results_track = list()
 
